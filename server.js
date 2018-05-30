6dd874ccf1d3ec50bbafb95ff3115e77fd521337
Lisa Canini <lisaceedesign@gmail.com>
2018-05-29 20:30:42 -0700

    model and server setup

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-const server = express();
+mongoose.connect('mongodb://localhost:27017/frienddb')
+  .then(mongo => {
+    console.log('connected');
+  })
+  .catch(err => {
+    console.log('error', err)
+  })
+
+const friendController = require('./friends/friendController');
+
+const server = express();  
 
 server.use(helmet());
 server.use(cors());
@@ -11,6 +22,9 @@ server.use(express.json());
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
+
+// server.use('/api/friends', friendController);
+//crashes server
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
